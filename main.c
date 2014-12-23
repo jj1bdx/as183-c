456ca937d9a065ca40def58fd34233ca853a49c2
Kenji Rikitake <kenji.rikitake@acm.org>
2014-12-23 10:08:32 +0900

    Add MIT license to main.c and main2.c

--- main.c
+++ main.c
@@ -1,3 +1,28 @@
+/*
+ * AS183 internal state loop testing code.
+ * License: MIT license as follows:
+ *
+ * Copyright (c) 2014 Kenji Rikitake.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 #include <stdint.h>
 #include <stdio.h>
 #include <inttypes.h>
@@ -26,12 +51,12 @@ int main(int argc, char *argv[]) {
         y3 = (x3 * 170) % PRIME3;
         count++;
         if (count % (uint64_t)INTERVAL == 0) {
-            printf("count = %" PRIu64 ", y1 = %u, y2=%u, y3 = %u\n",
+            printf("count = %" PRIu64 ", y1 = %u, y2 = %u, y3 = %u\n",
                     count, y1, y2, y3);
         }
         if ((y1 == INIT1) && (y2 == INIT2) && (y3 == INIT3)) {
             printf("Internal state loop detected\n");
-            printf("count = %" PRIu64 ", y1 = %u, y2=%u, y3 = %u\n",
+            printf("count = %" PRIu64 ", y1 = %u, y2 = %u, y3 = %u\n",
                     count, y1, y2, y3);
             keeploop = 0;
             continue;
--- main2.c
+++ main2.c
@@ -1,3 +1,32 @@
+/*
+ * AS183 internal state dumping code.
+ * License: MIT license as follows:
+ *
+ * Copyright (c) 2014 Kenji Rikitake.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal
+ * in the Software without restriction, including without limitation the
+ * rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 #include <stdint.h>
 #include <stdio.h>
 #include <inttypes.h>
@@ -25,7 +54,7 @@ int main(int argc, char *argv[]) {
         y2 = (x2 * 172) % PRIME2;
         y3 = (x3 * 170) % PRIME3;
         count++;
-        printf("count = %" PRIu64 ", y1 = %u, y2=%u, y3 = %u\n",
+        printf("count = %" PRIu64 ", y1 = %u, y2 = %u, y3 = %u\n",
                 count, y1, y2, y3);
         x1 = y1;
         x2 = y2;
