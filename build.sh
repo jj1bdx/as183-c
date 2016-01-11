a7e0eedfddf7e8e43a8da26ed0e0497d8c7c2114
Kenji Rikitake <kenji.rikitake@acm.org>
2016-01-11 23:09:49 +0900

    Add main3.c

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 cc -O3 -o main main.c
 cc -o main2 main2.c
+cc -O3 -o main3 main3.c
---
+++ main3.c
@@ -0,0 +1,80 @@
+/*
+ * AS183 internal state loop testing and truncation testing code.
+ * License: MIT license as follows:
+ *
+ * Copyright (c) 2016 Kenji Rikitake.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#include <stdint.h>
+#include <stdio.h>
+#include <inttypes.h>
+#include <math.h>
+
+#define PRIME1 (30269)
+#define PRIME2 (30307)
+#define PRIME3 (30323)
+
+#define INIT1 (3172)
+#define INIT2 (9814)
+#define INIT3 (20125)
+
+#define INTERVAL (100000000)
+
+int main(int argc, char *argv[]) {
+    uint16_t x1 = INIT1;
+    uint16_t x2 = INIT2;
+    uint16_t x3 = INIT3;
+    uint16_t y1, y2, y3;
+    uint64_t count = 0;
+    int keeploop = 1;
+    double r, r2;
+
+    while (keeploop) {
+        y1 = (x1 * 171) % PRIME1;
+        y2 = (x2 * 172) % PRIME2;
+        y3 = (x3 * 170) % PRIME3;
+        count++;
+        if (count % (uint64_t)INTERVAL == 0) {
+            printf("count = %" PRIu64 " y1 = %u y2 = %u y3 = %u r2 = %e\n",
+                    count, y1, y2, y3, r2);
+        }
+        r = (y1 / (double) PRIME1) +
+            (y2 / (double) PRIME2) +
+            (y3 / (double) PRIME3);
+        r2 = r - trunc(r);
+        if (fabs(r2) < 1.0e-10) {
+            printf("fabs(r2) < 1.0e-10\n");
+            printf("count = %" PRIu64 " y1 = %u y2 = %u y3 = %u r2 = %e\n",
+                    count, y1, y2, y3, r2);
+            continue;
+        }
+        if ((y1 == INIT1) && (y2 == INIT2) && (y3 == INIT3)) {
+            printf("Internal state loop detected\n");
+            printf("count = %" PRIu64 " y1 = %u y2 = %u y3 = %u\n",
+                    count, y1, y2, y3);
+            keeploop = 0;
+            continue;
+        }
+        x1 = y1;
+        x2 = y2;
+        x3 = y3;
+    }
+}
